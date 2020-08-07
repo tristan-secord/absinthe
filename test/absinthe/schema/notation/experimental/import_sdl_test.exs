6eb80e4f2b92ea26de1e2ede7f6218c6a6127764
Tristan Secord <tristan.secord@gmail.com>
2020-08-07 14:29:54 -0400

    Remove old telemetry tests

--- test/absinthe/schema/notation/experimental/import_sdl_test.exs
+++ test/absinthe/schema/notation/experimental/import_sdl_test.exs
@@ -570,45 +570,4 @@ defmodule Absinthe.Schema.Notation.Experimental.ImportSdlTest do
       Code.eval_string(schema)
     end)
   end
-
-  def handle_event(event, measurements, metadata, config) do
-    send(self(), {event, measurements, metadata, config})
-  end
-
-  describe "telemetry" do
-    setup context do
-      :telemetry.attach_many(
-        context.test,
-        [
-          [:absinthe, :resolve, :field, :start],
-          [:absinthe, :resolve, :field, :stop],
-          [:absinthe, :execute, :operation, :start],
-          [:absinthe, :execute, :operation, :stop]
-        ],
-        &__MODULE__.handle_event/4,
-        %{}
-      )
-
-      on_exit(fn ->
-        :telemetry.detach(context.test)
-      end)
-
-      :ok
-    end
-
-    test "executes on SDL defined schemas" do
-      assert {:ok,
-              %{data: %{"posts" => [%{"upcasedTitle" => "FOO"}, %{"upcasedTitle" => "BAR"}]}}} =
-               Absinthe.run(@query, Definition)
-
-      assert_receive {[:absinthe, :execute, :operation, :start], _, %{id: id}, _config}
-      assert_receive {[:absinthe, :execute, :operation, :stop], measurements, %{id: ^id}, _config}
-
-      assert_receive {[:absinthe, :resolve, :field, :start], measurements,
-                      %{resolution: %{definition: %{name: "posts"}}}, config}
-
-      assert_receive {[:absinthe, :resolve, :field, :stop], measurements,
-                      %{resolution: %{definition: %{name: "posts"}}}, config}
-    end
-  end
 end
